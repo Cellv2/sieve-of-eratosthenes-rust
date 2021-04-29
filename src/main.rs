52a0070ae1081d10a6ebfec1a90d8a8f920b601a
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-29 09:58:09 +0100

    Add fn to generate primes to x value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let target_num: i32 = 50;
+
+    let mut primes: Vec<i32> = Vec::new();
+    // 2 is the first prime number, so we start there
+    let mut nums_to_check: Vec<i32> = (2..target_num).collect::<Vec<i32>>();
+
+    for n in 2..target_num {
+        if nums_to_check.contains(&n) {
+            primes.push(n);
+            nums_to_check = nums_to_check
+                .into_iter()
+                .filter(|num| *num % n != 0)
+                .collect();
+        }
+    }
+
+    let output = primes
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    println!("{}", output);
 }
