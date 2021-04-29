35547934ec6dce317a1b52d27bc29aff3811543e
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-04-29 14:00:09 +0100

    Allow user input for the max number

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-fn main() {
-    let target_num: i32 = 50;
+use std::process;
+use std::{env, result::Result};
+
+fn run_app() -> Result<(), ()> {
+    // skip args[0] as that's always the target .exe
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len().ne(&1) {
+        eprintln!("error: Please enter exactly one number to test to");
+        return Ok(());
+    }
+
+    // let target_num: i32 = 50;
+    let target_num: i32 = args[0].parse::<i32>().unwrap();
 
     let mut primes: Vec<i32> = Vec::new();
     // 2 is the first prime number, so we start there
@@ -26,4 +37,16 @@ fn main() {
     println!("Calculating primes up to {}...", target_num);
     println!("Largest prime: {:?}", largest_prime.unwrap());
     println!("All primes: {}", all_primes);
+
+    return Ok(());
+}
+
+fn main() {
+    process::exit(match run_app() {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("error: {:?}", e);
+            1
+        }
+    });
 }
